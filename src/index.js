ee989006dfa65c3173af15f7ebe630d749895d9c
육기통 <yukkitong@yuggitong-ui-MacBook-Pro.local>
2018-06-17 16:46:28 +0900

    update

--- src/index.js
+++ src/index.js
@@ -1,15 +1,39 @@
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 
-const store = createStore((state = "No words...", action) => {
-  if (action.type === "SAY_HELLO") {
-    return "Hello World!";
+const reducer = (state = '', action) => {
+  switch (action.type) {
+    case 'SIMPLE_ACTION':
+      return state.concat(',', 'Received SIMPLE_ACTION');
+    default:
+      return state;
   }
-  return state;
-});
+};
 
-const unsubscribe = store.subscribe(() => console.log(store.getState()));
+const logger1 = ({ getState }) => next => action => {
+  console.log(`logger1 action : ${ action.type }`);
+  const returnValue = next(action);
+  console.log(`logger1 state : ${ getState() }`);
+  return returnValue;
+};
 
-store.dispatch({ type: "UNKNOWN" });
-store.dispatch({ type: "SAY_HELLO" });
+const logger2 = ({ getState, dispatch }) => next => action => {
+  console.log(`logger2 action : ${ action.type }`);
+  const returnValue = next(action);
+  console.log(`logger2 state : ${ getState() }`);
+  return returnValue;
+};
 
-unsubscribe();
+const store = createStore(reducer, applyMiddleware(logger1, logger2));
+// console.log(store.getState());
+// const un = store.subscribe(() => {
+//   console.log(`Final state : ${ store.getState() }`);
+// });
+
+console.log(store.getState());
+
+
+store.dispatch({ type: 'SIMPLE_ACTION' });
+
+// un();
+
+console.log(store.getState());
